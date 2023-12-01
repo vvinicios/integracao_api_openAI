3ff2118a7644fa5494da2db388def77661ddda05
Vinicios Ramos <viniciosramos0123@gmail.com>
2023-12-01 15:24:55 -0300

    Primeiro Commit

---
+++ integracao_api.py
@@ -0,0 +1,31 @@
+import openai
+import dotenv
+import os
+
+# Idenficar api_key
+dotenv.load_dotenv()
+openai.api_key = os.getenv("openai_api_key")
+
+# Testando resposta
+resposta = openai.chat.completions.create(
+  model = "gpt-3.5-turbo",
+    messages = [
+      {
+       "role" : "system",
+         "content" : "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
+      },
+      {
+       "role" : "user",
+         "content" : "Gere 5 produtos"
+         }
+    ],
+
+temperature=1,
+max_tokens=256,
+top_p=1,
+frequency_penalty=0,
+presence_penalty=0
+
+)
+
+print(resposta)
